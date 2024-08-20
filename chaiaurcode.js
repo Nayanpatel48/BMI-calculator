498b18952ccc3a4bf70e86a60be66eb04218c82a
nayan <www.xil187@gmail.com>
2024-08-20 22:31:39 +0530

    message component done

--- chaiaurcode.js
+++ chaiaurcode.js
@@ -9,14 +9,22 @@ form.addEventListener('submit', function (e) {
   const height = parseInt(document.querySelector('#height').value);
   const weight = parseInt(document.querySelector('#weight').value);
   const results = document.querySelector('#results');
-  const resultsSentence = document.querySelector('#results-sentence');
+  const message = document.querySelector('#message');
+
+  const bmi = (weight / ((height * height)/10000))
 
   if (height == '' || height < 0 || isNaN(height)) {
     results.innerHTML = `Please give a valid height.`;
   } else if (weight == '' || weight < 0 || isNaN(weight)) {
     results.innerHTML = `Please give a valid weight.`;
   } else {
-    const bmi = weight / (height * height);
     results.innerHTML = `<span>${bmi.toFixed(4)}</span>`;
+  }
+  if(bmi<18.6){
+    message.innerHTML = `<span>Underweight</span>`
+  } else if(bmi>18.6 && bmi<24.9){ 
+    message.innerHTML = `<span>Normalweight</span>`
+  } else {
+    message.innerHTML = `<span>Overweight</span>`
   }
 });
